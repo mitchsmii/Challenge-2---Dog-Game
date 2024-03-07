c1a46a89d07d8dc4a50ae972c970ebd041755159
mitchsmii <mitch.smi45@gmail.com>
2024-03-07 14:10:14 -0800

    Finished Project

---
+++ Dog Game/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float timer = 1f;
+    public bool onCooldown;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && onCooldown == false)
+        {
+            spawnDog();
+            Invoke("cooldown", timer);
+            onCooldown = true;
+        }
+    }
+
+    public void spawnDog()
+    {
+        Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+    }
+
+    public void cooldown()
+    {
+        onCooldown = false;
+    }
+}
